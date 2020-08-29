28409f9ff5419e54c7a6016a4d94bad809f5b86e
hhhhaeun <jse03310@gmail.com>
2020-08-29 18:28:14 +0900

    BOJ C++ 11057 오르막 수

---
+++ 11057.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int mod = 10007;
+int d[1001][1001];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i <= 9; i++)
+		d[1][i] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			for (int k = 0; k <= j; k++)
+			{
+				d[i][j] += d[i - 1][k];
+				d[i][j] %= mod;
+			}
+		}
+	}
+	long long sum = 0;
+	for (int i = 0; i <= 9; i++)
+		sum += d[n][i];
+	cout << sum % mod << endl;
+
+}
