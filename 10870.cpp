11c265d8b358a4cfeac517d91cb675d2739f2afa
hhhhaeun <jse03310@gmail.com>
2020-09-03 03:13:04 +0900

    BOJ C++ 10870 피보나치 수 5

---
+++ 10870.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int d[21];
+
+int main()
+{
+	int n;
+	cin >> n;
+	d[0] = 0;
+	d[1] = 1;
+	for (int i = 2; i <= n; i++)
+		d[i] = d[i - 1] + d[i - 2];
+	cout << d[n] << "\n";
+}
